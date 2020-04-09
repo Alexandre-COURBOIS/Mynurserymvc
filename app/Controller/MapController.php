ff19a11781c715acd93db285dda8f82239ebb38e
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2020-04-09 11:49:43 +0200

    adding a crecheModel and MapConstroller

---
+++ app/Controller/MapController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Controller;
+
+
+class MapController extends Controller
+{
+
+    public function Map()
+    {
+
+    }
+
+    public function MapRequest()
+    {
+
+    }
+}
