22a13601d89b3ea8fe27d5bbf9924adbf2e2390f
pierrehlvt <pierre.halavent@gmail.com>
2020-04-09 17:03:18 +0200

    routes
    - dumb nurses

---
+++ view/app/default/nurse/nurses.php
@@ -0,0 +1,11 @@
+<div class="container">
+
+        <?php
+        foreach ($creches as $creche) {
+            echo '<pre>';
+            print_r($creche);
+            echo '</pre>';
+        }
+        ?>
+
+</div>
