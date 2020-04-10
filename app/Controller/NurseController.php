31f07a156d89aa4e7ac1b3288e3cabc2f0364b87
pierrehlvt <pierre.halavent@gmail.com>
2020-04-10 16:23:44 +0200

    NurseController
    - getting coordinates in $_SESSION
    - function calculateDistance to calculate the distance between nurse and user house
    - display nurses that are under 30 km

--- app/Controller/NurseController.php
+++ app/Controller/NurseController.php
@@ -5,16 +5,23 @@ namespace App\Controller;
 
 use App\Weblitzer\Controller;
 use App\Model\CrecheModel;
+use App\Model\ProfilModel;
 use App\Weblitzer\Model;
 
 class NurseController extends Controller
 {
     public function nurse() {
+
         $titre = 'Liste de toutes les crèches';
         $creches = CrecheModel::All();
+
+
+
         $this->render('app.default.nurse.nurses',array(
             'titre' => $titre,
             'creches' => $creches,
         ));
     }
+
+
 }
