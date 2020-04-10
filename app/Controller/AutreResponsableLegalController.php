3c640e0c3bec4ca93342340f8b4a3cb347a9dd8d
stephane <github@lecuyer.eu>
2020-04-10 15:30:45 +0200

    Form to add a person in charge with error management. Set up of the page more description in order to make the link and retrieve the ID of the child in the $_GET

--- app/Controller/AutreResponsableLegalController.php
+++ app/Controller/AutreResponsableLegalController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use App\Model\EnfantModel;
 use App\Weblitzer\Controller;
 use App\Model\AutreResponsableLegalModel;
 use App\Service\Form;
@@ -13,6 +14,20 @@ class AutreResponsableLegalController extends Controller
 {
     public function ajout()
     {
+        $errors = array();
+        if (!empty($_POST['submitted'])){
+            $post = $this->cleanXss($_POST);
+            $valid = new Validation();
+            $errors['nom_autreResp'] = $valid->textValid($post['nom_autreResp'], 'son nom', 2, 20);
+            $errors['prenom_autreResp'] = $valid->textValid($post['prenom_autreResp'], 'son prénom', 2, 20);
+            $errors['role_autreResp'] = $valid->selectValid($post['role_autreResp'], 'un rôle');
+            if ($valid->IsValid($errors)){
 
+            }
+        }
+        $form = new Form($errors);
+        $this->render('app.default.autreResponsable.ajoutResponsable',array(
+            'form' => $form,
+        ));
     }
 }
--- config/routes.php
+++ config/routes.php
@@ -4,10 +4,10 @@ $routes = array(
     array('home','default','index'),
     array('ajoutEnfant','enfant','ajoutEnfant'),
     array('editProfil','profil','edit'),
-
     array('reservation','reservation','reservation'),
     array('editEnfant','enfant','editEnfant', array('id')),
     array('deleteEnfant','enfant','deleteEnfant', array('id')),
+    array('ajoutResponsable','autreResponsableLegal','ajout'),
 );
 
 
