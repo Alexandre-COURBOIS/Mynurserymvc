8155216d719a211dfd5d614fad9353b19cebcef5
pierrehlvt <pierre.halavent@gmail.com>
2020-04-19 11:39:44 +0200

    RateModel
    - add RateModel
    - getters and setters
    - method rateNurse

---
+++ app/Model/RateModel.php
@@ -0,0 +1,53 @@
+<?php
+
+
+namespace App\Model;
+
+
+use App\App;
+use App\Weblitzer\Model;
+
+
+class RateModel extends Model
+{
+    protected static $table = 'nurs_notes';
+    protected $id_user;
+    protected $id_creche;
+
+    /**
+     * @return mixed
+     */
+    public function getIdUser()
+    {
+        return $this->id_user;
+    }
+
+    /**
+     * @param mixed $id_user
+     */
+    public function setIdUser($id_user): void
+    {
+        $this->id_user = $id_user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdCreche()
+    {
+        return $this->id_creche;
+    }
+
+    /**
+     * @param mixed $id_creche
+     */
+    public function setIdCreche($id_creche): void
+    {
+        $this->id_creche = $id_creche;
+    }
+
+    public static function rateNurse($id_user, $id_creche, $note) {
+        $sql = "INSERT INTO " . self::getTable() . " VALUES(NULL, ?, ?, ?)";
+        return App::getDatabase()->prepareInsert($sql, [$id_user, $id_creche, $note]);
+    }
+}
