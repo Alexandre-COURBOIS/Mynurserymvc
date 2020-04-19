23e9582cb4f72a2c1b69645f8506dd76c5a36138
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2020-04-19 17:05:58 +0200

    hotfix on placment function

--- view/app/default/nurse/nurses.php
+++ view/app/default/nurse/nurses.php
@@ -24,22 +24,6 @@
 
             </div>
             <?php
-//print_r($creche);
-
-            function calculateDistance($lat1, $lng1, $lat2, $lng2, $miles = false)
-            {
-                $earth_radius = 6378137;   // Terre = sphère de 6378km de rayon
-                $rlo1 = deg2rad($lng1);
-                $rla1 = deg2rad($lat1);
-                $rlo2 = deg2rad($lng2);
-                $rla2 = deg2rad($lat2);
-                $dlo = ($rlo2 - $rlo1) / 2;
-                $dla = ($rla2 - $rla1) / 2;
-                $a = (sin($dla) * sin($dla)) + cos($rla1) * cos($rla2) * (sin($dlo) * sin(
-                            $dlo));
-                $d = 2 * atan2(sqrt($a), sqrt(1 - $a));
-                return ($earth_radius * $d);
-            }
 
         } else {
             header('Location: http://localhost/mynursery');
@@ -51,3 +35,17 @@
     header('Location: http://localhost/mynursery');
 }
 
+function calculateDistance($lat1, $lng1, $lat2, $lng2, $miles = false)
+{
+    $earth_radius = 6378137;   // Terre = sphère de 6378km de rayon
+    $rlo1 = deg2rad($lng1);
+    $rla1 = deg2rad($lat1);
+    $rlo2 = deg2rad($lng2);
+    $rla2 = deg2rad($lat2);
+    $dlo = ($rlo2 - $rlo1) / 2;
+    $dla = ($rla2 - $rla1) / 2;
+    $a = (sin($dla) * sin($dla)) + cos($rla1) * cos($rla2) * (sin($dlo) * sin(
+                $dlo));
+    $d = 2 * atan2(sqrt($a), sqrt(1 - $a));
+    return ($earth_radius * $d);
+}
