5e05f7af69c2a3c67ae8d74353e3e31764c65123
pierrehlvt <pierre.halavent@gmail.com>
2020-04-19 12:15:01 +0200

    singlenurse
    - display average rating

--- view/app/default/nurse/singlenurse.php
+++ view/app/default/nurse/singlenurse.php
@@ -1,10 +1,12 @@
 <?php
+
 $html = '<ul class="list-group">';
 $html .= '<li class="list-group-item">' . $creche[0]->nom_creche . '</li>';
 $html .= '<li class="list-group-item">' . $creche[0]->nom_gerant . ' ' . $creche[0]->prenom_gerant . '</li>';
 $html .= '<li class="list-group-item">' . $creche[0]->email . '</li>';
 $html .= '<li class="list-group-item">' . $creche[0]->telephone . '</li>';
 $html .= '<li class="list-group-item">' . $creche[0]->num_rue . ' ' . $creche[0]->nom_rue . ' ' . $creche[0]->codepostal . ' ' . $creche[0]->ville . '</li>';
+$html .= '<li class="list-group-item">Les autres utilisateurs ont noté ' . $note . '/5 cette crèche';
 echo $html;
 ?>
 <form action="" method="post">
