c63c4fe61d7b7f7655a9a37c66f35ff510667914
pierrehlvt <pierre.halavent@gmail.com>
2020-04-19 18:13:09 +0200

    Nurseall
    - add link to redirect on singlenurse

--- view/app/default/nurse/nurseall.php
+++ view/app/default/nurse/nurseall.php
@@ -2,6 +2,6 @@
 echo '<h2>' . $titre .  '</h2>';
 echo '<ul class="list-group">';
 foreach ($creches as $creche) {
-    echo '<li class="list-group-item">' . $creche->nom_creche . '</li>';
+    echo '<li class="list-group-item"><a href="'. $view->path('singlenurse', array($creche->id_creche)) .'"> ' . $creche->nom_creche . '</a></li>';
 }
 echo '</ul>';
