e831f083a0fed8c2f81295728c035530f3ead63a
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2020-04-19 18:22:04 +0200

    Hotfix on closing foreach, who was break on merge

--- view/app/default/nurse/nurseall.php
+++ view/app/default/nurse/nurseall.php
@@ -1,15 +1,16 @@
 <?php if (!empty($_SESSION)) {
     if (!empty($_SESSION['login'])) {
-        if ($_SESSION['login']['user'] === "particulier") { ?>
-                        
-echo '<h2>' . $titre .  '</h2>';
-echo '<ul class="list-group">';
-foreach ($creches as $creche) {
-    echo '<li class="list-group-item"><a href="'. $view->path('singlenurse', array($creche->id_creche)) .'"> ' . $creche->nom_creche . '</a></li>';
-            echo '</ul>';
+        if ($_SESSION['login']['user'] === "particulier") {
+
+            echo '<h2>' . $titre . '</h2>';
+            echo '<ul class="list-group">';
+            foreach ($creches as $creche) {
+                echo '<li class="list-group-item"><a href="' . $view->path('singlenurse', array($creche->id_creche)) . '"> ' . $creche->nom_creche . '</a></li>';
+                echo '</ul>';
+            }
 
 
-       <?php } else {
+        } else {
             header('Location: http://localhost/mynursery');
         }
     } else {
