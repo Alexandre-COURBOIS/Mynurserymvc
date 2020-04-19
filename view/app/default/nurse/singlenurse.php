fc95332d8e50ddc9732460131b17b648adf7f33f
pierrehlvt <pierre.halavent@gmail.com>
2020-04-19 11:38:42 +0200

    singlenurse
    - adding a select to rate a nurse

--- view/app/default/nurse/singlenurse.php
+++ view/app/default/nurse/singlenurse.php
@@ -1,5 +1,4 @@
 <?php
-print_r($creche);
 $html = '<ul class="list-group">';
 $html .= '<li class="list-group-item">' . $creche[0]->nom_creche . '</li>';
 $html .= '<li class="list-group-item">' . $creche[0]->nom_gerant . ' ' . $creche[0]->prenom_gerant . '</li>';
@@ -7,3 +6,14 @@ $html .= '<li class="list-group-item">' . $creche[0]->email . '</li>';
 $html .= '<li class="list-group-item">' . $creche[0]->telephone . '</li>';
 $html .= '<li class="list-group-item">' . $creche[0]->num_rue . ' ' . $creche[0]->nom_rue . ' ' . $creche[0]->codepostal . ' ' . $creche[0]->ville . '</li>';
 echo $html;
+?>
+<form action="" method="post">
+    <select name="rate">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+    </select>
+    <input type="submit" name="submitted" class="btn btn-success" value="Noter!">
+</form>
